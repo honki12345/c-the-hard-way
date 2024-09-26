feb592b55d4109470f2d8f0d8f1e8bfe6eb2fd29
hookim <honki12@naver.com>
2024-09-26 22:53:22 +0900

    ~ex10

---
+++ Makefile
@@ -0,0 +1,8 @@
+CFLAGS=-Wall -g
+
+all: ex1 ex3 ex7 ex10
+
+clean:
+	rm -f ex1
+	rm -f ex3
+	rm -f ex7
---
+++ ex2/ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* 이것은 주석이다. */
+int main(int argc, char const *argv[])
+{
+    int distance = 100;
+
+    // 이것도 주석이다.
+    printf("You are %d miles away.\n", distance);
+    printf("wefwefwewefwe \nwefwefwefwe \nwefwefwe \nfwefwefwe \nfwefwe\n");
+
+    return 0;
+}
